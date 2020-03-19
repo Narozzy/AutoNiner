bde015af0e194ef3b54861592d2459e89aa6cad5
Noah Rozelle <nrozelle@uncc.edu>
2020-03-19 14:37:16 -0400

    Working on argument parsing

--- autoniner/utils/excel_automation.py
+++ autoniner/utils/excel_automation.py
@@ -3,6 +3,16 @@ from openpyxl import Workbook, load_workbook
 import fire
 from collections import OrderedDict
 
+SUPPORTED_FUNCTIONS = {
+    'AVERAGE': lambda x: (sum(x))/len(x),
+    'DIVISION': lambda x,divisor: x/divisor,
+
+}
+
+SUPPORTED_MODIFIERS = {
+    'spaced': lambda x: ' '.join(x)
+}
+
 """ @description: to accept a xlsx/csv file and modify the data based on the format encoded within """
 def construct_template_headers(ws):
     """ @description: returns an OrderedDict of headers/the encoded value """
@@ -11,12 +21,24 @@ def construct_template_headers(ws):
         d.update({header.value:encoding.value})
     return d
 
+def parse_item(i):
+    it = i[2:-2] # remove << >>
+    if 'FORM' in it:
+        it = it[5:-1]
+        args = it.split(',')
+    else:
+        args = it.split(':')
+    return args
+
+
 def main():
     wb_template = load_workbook('template.xlsx')
     wb_data = pd.read_excel('mock_data.xlsx')
     ws_template = wb_template.active
     forms = construct_template_headers(ws_template)
-    
+    for col,item in forms.items():
+        a = parse_item(item)
+        print()
 
     print()
 
