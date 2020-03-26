10c101bac7319ddb5756cf8a61d51e77be838040
Noah Rozelle <nrozelle@uncc.edu>
2020-03-26 14:49:55 -0400

    Created task model and table, next step is to make a category group that task can become part of.

--- autoniner/home/models.py
+++ autoniner/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField('Task Title',max_length=50)
+    description = models.CharField('Task Description',max_length=50, blank=True)
+
+    # Administrative log information
+    task_id = models.AutoField(primary_key=True) # This autoincrements, so we can guarentee uniqueness on this field.
+    date_created = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """ This is strictly optional, this just makes our lives easier when working with the ORM for this model. """
+        ordering = ['date_created']
+        verbose_name_plural = 'tasks'
+        get_latest_by = ['date_created']
