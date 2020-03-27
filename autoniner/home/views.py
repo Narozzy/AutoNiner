16d6c529689ec7fb1b2b27b06dfefb8149141801
Noah Rozelle <nrozelle@uncc.edu>
2020-03-27 15:49:22 -0400

    Got task form completion done

--- autoniner/home/views.py
+++ autoniner/home/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', context={'hello': 'world'})
 
 def CreateTask(request):
+    content = {'current_user': request.user}
     if request.method == 'POST':
-        # This will be after they have completed the form
-        pass
+        print(request.POST)
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'index.html', {})
     elif request.method == 'GET':
         # This will retrieve the form
-        pass
-    return render(request, 'create_task.html', context={'form': 'task'})
+        content.update({'form': TaskForm()})
+    return render(request, 'create_task.html', context=content)
