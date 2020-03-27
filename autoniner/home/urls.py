6478c72117efdf91b5ce6cfde58b8c44e119ff02
Noah Rozelle <nrozelle@uncc.edu>
2020-03-27 14:35:27 -0400

    Added superuser, will post credentials in discord. Working on getting form for task creation done

--- autoniner/home/urls.py
+++ autoniner/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('home/', views.index),
+    path('home/', views.index, name='home'),
+    path('home/task/new', views.CreateTask, name='newTask'),
 ]
--- autoniner/home/views.py
+++ autoniner/home/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html', context={'hello': 'world'})
+
+def CreateTask(request):
+    if request.method == 'POST':
+        # This will be after they have completed the form
+        pass
+    elif request.method == 'GET':
+        # This will retrieve the form
+        pass
+    return render(request, 'create_task.html', context={'form': 'task'})
